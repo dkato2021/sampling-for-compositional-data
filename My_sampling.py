f85a619e80cd6c2e1891e7318fd6c3d1137d7912
buncho <78598272+buncho1021@users.noreply.github.com>
2021-10-25 17:48:47 +0900

    Update My_sampling.py

--- My_sampling.py
+++ My_sampling.py
@@ -9,9 +9,10 @@ def fix_seed(seed):
     np.random.seed(seed)
    
 class My_sampling(object):
-    def __init__(self, num_col = None, num_sample = None):
+    def __init__(self, num_col = None, num_sample = None, dist = None):
         self.num_col = num_col
         self.num_sample = num_sample
+        self.dist = dist
         self.dfW = pd.DataFrame()
         self.dfN = pd.DataFrame()
         self.dfP = pd.DataFrame()
@@ -33,17 +34,20 @@ class My_sampling(object):
             fix_seed(i) ;tmp = pd.DataFrame([np.random.poisson(lam=50) for j in range(self.num_sample)], columns =[f"p{i}"])
             self.dfP = pd.concat([self.dfP, tmp], axis = 1)
         return self.dfP
-    
-    def get_X(self, dist = None): #単体空間への写像
-        if dist == 0: #一様乱数
-            W = self.get_W()
-        elif dist == 1: #正規分布
-            W = self.get_N()  
-        elif dist == 2: #ポアソ分布
-            W = self.get_P()  
+
+    def get_R(self):
+        if self.dist == 0:
+            return self.get_W()
+        elif self.dist == 1:
+            return self.get_N()
+        elif self.dist == 2:
+            return self.get_P()
+        
+    def get_X(self):#単体空間への写像
+        R = self.get_R()
         _df = pd.DataFrame()
         for i in range(self.num_sample):
-            tmp = [W.iloc[i,j]/sum(W.loc[i,:]) for j in range(self.num_col)]
+            tmp = [R.iloc[i,j]/sum(R.loc[i,:]) for j in range(self.num_col)]
             _df = pd.concat([_df, pd.DataFrame(tmp).T], axis = 0)
         _df.index = range(self.num_sample) ;_df.columns = [f"x{i}" for i in range(self.num_col)]
         return _df
@@ -55,8 +59,8 @@ def plot(df):
     sns.scatterplot(data=df, x=df.columns[1], y = df.columns[2], ax = ax3);plt.show()
     
 if __name__ == "__main__":
-    instance = My_sampling(num_col = 3, num_sample = 300)
-    W, N, P = instance.get_W(), instance.get_N(), instance.get_P()
-    X = instance.get_X(dist = 1)
-    
-    plot(N) ;plot(X)
+    instance = My_sampling(num_col = 5, num_sample = 300, dist = 2)
+    R = instance.get_R()
+    X = instance.get_X()#単体空間への写像
+
+    plot(R) ;plot(X)
