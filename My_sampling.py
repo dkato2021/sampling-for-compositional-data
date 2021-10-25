d9da69e6e8a1f9f12e956acbf991cc0439cbf1a5
buncho <78598272+buncho1021@users.noreply.github.com>
2021-10-25 15:02:18 +0900

    Create My_sampling.py

---
+++ My_sampling.py
@@ -0,0 +1,44 @@
+import  os, sys
+import random
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+def fix_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+   
+class My_sampling(object):
+    def __init__(self, num_col = None, num_sample = None):
+        self.num_col = num_col
+        self.num_sample = num_sample
+        self.dfW = pd.DataFrame()
+        self.dfN = pd.DataFrame()
+        
+    def get_W(self): #実空間からランダムサンプリング
+        for i in range(self.num_col):
+            fix_seed(i) ;tmp = pd.DataFrame([random.uniform(0, 1) for j in range(self.num_sample)], columns =[f"w{i}"])
+            self.dfW = pd.concat([self.dfW, tmp], axis = 1)
+        return self.dfW
+    
+    def get_N(self): #実空間から正規分布に従ったサンプリング
+        for i in range(self.num_col):
+            fix_seed(i) ;tmp = pd.DataFrame([np.random.normal(0, 1) for j in range(self.num_sample)], columns =[f"n{i}"])
+            self.dfN = pd.concat([self.dfN, tmp], axis = 1)
+        return self.dfN
+    
+    def get_X(self, norm = None): #単体空間への写像
+        if norm:
+            W = self.get_N()
+        else:
+            W = self.get_W()  
+        _df = pd.DataFrame()
+        for i in range(self.num_sample):
+            tmp = [W.iloc[i,j]/sum(W.loc[i,:]) for j in range(self.num_col)]
+            _df = pd.concat([_df, pd.DataFrame(tmp).T], axis = 0)
+        _df.index = range(self.num_sample) ;_df.columns = [f"x{i}" for i in range(self.num_col)]
+        return _df
+    
+def plot(df, x = None, y = None):
+    fig, ax = plt.subplots(figsize=(5, 5))
+    sns.scatterplot(data=df, x=df.columns[x], y=df.columns[y]) ;plt.show()
